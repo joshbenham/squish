7d0f5cd09e2adf44e369911e7de4e40e85d7b3c6
Josh Benham <josh.benham@99designs.com>
2012-05-18 14:58:07 +1000

    Created a helper to test if a file is an image

--- squish/helpers.py
+++ squish/helpers.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*- #
 
 import envoy
+from PIL import Image
 
 def exists(string):
   return envoy.run("which %s" % string).status_code == 0
+
+def isimage(file):
+  try:
+    Image.open(file)
+    return True
+  except IOError:
+    return False
